1974e013abd3c243058601e3450631e7b4872327
Bernard Llanos <bernardllanos@cmail.carleton.ca>
2014-10-29 14:51:08 -0400

    Defined particle vertex type.

--- engine/header/geometry/vertexTypes.h
+++ engine/header/geometry/vertexTypes.h
@@ -34,5 +34,27 @@ struct SkinnedColorVertexType {
 	DirectX::XMFLOAT4 boneWeights;
 	DirectX::XMFLOAT3 position;
 	DirectX::XMFLOAT3 normal;
-	DirectX::XMFLOAT4 index; // Multi-purpose: Color, or 1D to 4D texture coordinates
+	DirectX::XMFLOAT4 index; // Multi-purpose: Color/albedo, or 1D to 4D texture coordinates
+};
+
+/*
+#define PARTICLEVERTEXTYPE_COMPONENTS 5
+struct ParticleVertexType {
+	DirectX::XMFLOAT3 position; // World-space position offset (at time zero) from a base position
+	DirectX::XMFLOAT3 right; // Non-normalized vector defining the view-space billboard plane
+	DirectX::XMFLOAT3 up; // Non-normalized vector defining the view-space billboard plane
+	DirectX::XMFLOAT3 linearVelocity; // Non-normalized world-space linear velocity vector
+	// DirectX::XMFLOAT4 angularVelocity; // World-space angular velocity quaternion
+	DirectX::XMFLOAT4 index; // Multi-purpose: Color/albedo, or 1D to 4D texture coordinates
+};
+*/
+
+#define PARTICLEVERTEXTYPE_COMPONENTS 5
+struct ParticleVertexType {
+	DirectX::XMFLOAT3 position; // World-space position offset (at time zero) from a base position
+	DirectX::XMFLOAT3 billboard;	// x = billboard width (view-space units),
+									// y = billboard height (view-space units),
+									// z = per-millisecond clockwise turn around view-space 'look' axis (radians)
+	DirectX::XMFLOAT3 linearVelocity; // Non-normalized world-space linear velocity vector
+	DirectX::XMFLOAT4 index; // Multi-purpose: Color/albedo, or 1D to 4D texture coordinates
 };
