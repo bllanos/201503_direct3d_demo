f3e794bfb6664f989cefcfbb17ee30d3e8f20324
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2014-11-22 14:33:01 -0500

    Draft implementation of Knot completed.

--- engine/header/physics/Knot.h
+++ engine/header/physics/Knot.h
@@ -15,7 +15,8 @@ Created November 21, 2014
 Primary basis: None
 
 Description
-  -Abstract class defining a knot between two segments in a Bezier curve
+  -Abstract class defining a C_1 continuous knot
+     between two segments in a Bezier curve
 */
 
 #pragma once
@@ -24,7 +25,7 @@ Description
 #include <DirectXMath.h>
 #include "Transformable.h"
 
-#define KNOT_DEFAULT_SPEED 1.0f;
+#define KNOT_DEFAULT_SPEED 1.0f
 
 class Knot {
 public:
@@ -39,11 +40,9 @@ public:
 
 protected:
 	/* A Knot can be created single or double-sided.
-	   This will determine which points
-	   are output by getControlPoints().
+	   This will determine which points are output by getControlPoints().
 
-	   If 'controlPoints' is not null, it will be used
-	   to set the initial control point values.
+	   'controlPoints' will be used to set the initial control point values.
 
 	   The contents of 'controlPoints' should be as follows, if 'side' is:
 	   -START: p0 and p1; The initial position and tangent control points of the second segment
@@ -53,7 +52,7 @@ protected:
 	   This object does not take ownership of the 'controlPoints'
 	   parameter, but copies all necessary data from it.
 	 */
-	Knot(const PointSet side, const DirectX::XMFLOAT3* const controlPoints = 0);
+	Knot(const PointSet side, const DirectX::XMFLOAT3* const controlPoints);
 
 	/* Similar to the Knot(PointSet, XMFLOAT3) constructor,
 	   but calculates initial control point values
@@ -90,25 +89,28 @@ public:
 	/* Outputs control points into 'controlPoints'.
 	   The control points which are output, and their order,
 	   is determined by the 'side' constructor parameter.
+
+	   'controlPoints' will be incremented to reflect the number
+	   of control points output.
 	 */
-	virtual HRESULT getControlPoints(DirectX::XMFLOAT4* const controlPoints, const PointSet side);
+	virtual HRESULT getControlPoints(DirectX::XMFLOAT4*& controlPoints);
 
 	/* The following helper functions are not called by this class. */
 protected:
-	HRESULT updateControlPoints(Transformable* const transform);
+	HRESULT updateControlPoints(Transformable& const transform);
 
 private:
-	HRESULT updateP0(Transformable* const transform);
-	HRESULT updateP3(Transformable* const transform);
-	HRESULT updateP1(Transformable* const transform);
-	HRESULT updateP2(Transformable* const transform);
+	HRESULT updateP2(Transformable& const transform);
+	HRESULT updateP3(Transformable& const transform);
+	HRESULT updateP0(Transformable& const transform);
+	HRESULT updateP1(Transformable& const transform);
 
 	// Data members
 private:
 	/* Determines which control points
 	   are allocated
 	 */
-	const PointSet* m_side;
+	const PointSet m_side;
 
 	DirectX::XMFLOAT3* m_p2; // Final tangent control point of the previous segment
 	DirectX::XMFLOAT3* m_p3; // Final position control point of the previous segment
